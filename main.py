ab535081d5f7b4d83e6a697732b376a5df5f1eeb
DerekAdol Ch.C <43061460+adolfochc@users.noreply.github.com>
2022-12-12 04:23:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def predict():
     #app.run()
     
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=8000))
 
 
 #pip install numpy
