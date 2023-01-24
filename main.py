47f4ea1deebc9034975971f7eb672d9d5cf47480
DerekAdol Ch.C <43061460+adolfochc@users.noreply.github.com>
2023-01-24 03:08:19 -0500

    update api

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ with open('decision_tree_model.pkl', 'rb') as model_file:
 with open("linear_regression_model.pkl", "rb") as f:
   model_lineal = pickle.load(f)
 
-with open("linear_regression.pkl", "rb") as fv:
+with open("arbol_regression.pkl", "rb") as fv:
   model_regression = pickle.load(fv)
 
 # Luego, creamos la función que se encargará de hacer predicciones
@@ -36,34 +36,63 @@ def hello():
 def predictv3():
     # Obtenemos los datos de entrada de la solicitud HTTP
     input_data = request.json
-    print("REQUEST => ",input_data)
+    #print("REQUEST => ",input_data)
+    # Leer la tabla externa
+    tabla_conversion_equipo = pd.read_excel("Equipo.xlsx")
+    tabla_conversion_material = pd.read_excel("Material.xlsx")
     # Crear un diccionario con los datos
-    datos = {'KmOT':[input_data['kmot']],'Fecha Prog.': [input_data['fecha']]}
+    #datos = {'KmOT':[input_data['kmot']],'Fecha Prog.': [input_data['fecha']]}
+    datos = {'Equipo': input_data['equipo'],'Material':input_data['material'],'KmOT':input_data['kmot']}
     # Crear la tabla a partir del diccionario
     #tabla = pd.DataFrame(datos)
-    tabla = pd.DataFrame(datos, columns=['KmOT','Fecha Prog.'])
-    tabla['Fecha Prog.'] = tabla['Fecha Prog.'].astype(np.datetime64)
-    tabla['Fecha Prog.'] = tabla['Fecha Prog.'].astype(np.int64)
+    #tabla = pd.DataFrame(datos, columns=['KmOT','Fecha Prog.'])
+    tabla = pd.DataFrame(datos, columns=['Equipo','Material','KmOT'])
+    #print(tabla.info())
+    #print(tabla)
+    #print("---------------------------------")
+    #print(tabla_conversion_equipo.info())
+    # Unir la tabla externa con el DataFrame original en base a la columna "valor" solo con las coincidencias y dejando solo la columna "id"
+    tabla = pd.merge(tabla, tabla_conversion_equipo[["valor", "id"]], left_on='Equipo', right_on="valor", how='inner').drop(columns=['valor'])
+    tabla = tabla.drop(['Equipo'], axis=1)
+    tabla = tabla.rename(columns={"id": "Equipo"})
+    #print(tabla.info())
+    tabla = pd.merge(tabla, tabla_conversion_material[["valor", "id"]], left_on='Material', right_on="valor", how='inner').drop(columns=['valor'])
+    tabla = tabla.drop(['Material'], axis=1)
+    tabla = tabla.rename(columns={"id": "Material"})
+    print(tabla.info())
+    tabla = tabla[['Equipo','Material','KmOT']]
+    print(tabla.info())
+
+    #tabla['Fecha Prog.'] = tabla['Fecha Prog.'].astype(np.datetime64)
+    #tabla['Fecha Prog.'] = tabla['Fecha Prog.'].astype(np.int64)
     
     # Generar un número aleatorio de días entre 1 y 30 (inclusive)
-    random_days = random.randint(30, 90)
+    #random_days = random.randint(30, 90)
     
-    print(tabla)
+    #print(tabla)
     predictions = model_regression.predict(tabla)
-
-    int64_series = pd.Series([predictions[0]])
-    datetime_series = pd.to_datetime(int64_series)
+    array_pred = np.array(predictions)
+    int64_series = pd.Series(array_pred)
+    datetime_series = pd.to_datetime(int64_series, unit='ns')
+    #int64_series = pd.Series([predictions[0]])
+    #datetime_series = pd.to_datetime(int64_series)
     print("Predicted:", datetime_series[0])
     #fecha_convertida = datetime_series[0].strftime("%Y-%m-%d %H:%M:%S")
     fecha_p = datetime_series[0]
     # Sumar el número aleatorio de días al objeto datetime
-    datetime_object_future = fecha_p +  datetime.timedelta(days=random_days)
+    #datetime_object_future = fecha_p +  datetime.timedelta(days=random_days)
 
-    fecha_convertida = datetime_object_future.strftime("%Y-%m-%d %H:%M:%S")
+    #fecha_convertida = datetime_object_future.strftime("%Y-%m-%d %H:%M:%S")
     #fecha_convertida = "error"
+    response_pred = datetime_series.dt.strftime('%Y-%m-%d %H:%M:%S')
+    print(response_pred)
+    response_pred_dict = response_pred.to_dict()
+    data_list = [{"fecha_predic": val} for val in response_pred_dict.values()]
+
+    #data_list = response_pred.tolist()
     response = jsonify(
         {
-            'data': fecha_convertida
+            'data': data_list
         }
     )
     return response
@@ -144,6 +173,7 @@ def predict():
     #app.run()
     
 if __name__ == '__main__':
+    #app.run(host="0.0.0.0",debug=True, port=os.getenv("PORT", default=3000))
     app.run(host="0.0.0.0",debug=True, port=os.getenv("PORT", default=8000))
 
 
